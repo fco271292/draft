4d907d4fe673623d6fe66d8cd4ea2b90f05c398b
Francisco Reyes <juan271292@hotmail.com>
2017-09-07 20:22:23 -0500

    Se agrega clase carro para generar una estructura de JSON mas completa

--- SpringAnotaciones/src/main/java/com/fco271292/springanotaciones/model/Carro.groovy
+++ SpringAnotaciones/src/main/java/com/fco271292/springanotaciones/model/Carro.groovy
@@ -3,8 +3,7 @@ package com.fco271292.springanotaciones.model
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
-class Persona implements Serializable {
+class Carro implements Serializable {
 	Integer id
-	String nombre
-	List<Carro> carro
+	String descripcion
 }
