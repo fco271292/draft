7d4ec1ef10882067a3d3a97e825e06a072b48ba7
juanimsoftware <juan.reyes@imsoftware.pro>
2017-09-21 17:46:10 -0500

    Se agrega validaciones

--- ejemplo_angular/js/ejemplo_angular.js
+++ ejemplo_angular/js/ejemplo_angular.js
@@ -6,3 +6,32 @@ app.controller('ControladorFecha', ['$scope', function($scope){
 		alert("Hola "+$scope.nombre)
 	};
 }]);
+
+app.controller('ValidacionSimple', ['$scope', function($scope){
+	$scope.longitud_minima = false
+	$scope.longitud_cadena_minima = 3
+	$scope.$watch('password', function(newValue, oldValue, scope) {
+		if (newValue != null && newValue.length < $scope.longitud_cadena_minima){
+			$scope.longitud_minima = true
+		}else{
+			$scope.longitud_minima = false
+		}
+	});
+}])
+
+app.controller('ValidacionMultiple', ['$scope', function($scope){
+	$scope.cambio = function () {
+		$scope.valorCheckbox = $scope.libro
+	}
+	$scope.$watchGroup(['libro','revista','valorSiNo'], function(newValue, oldValue, scope) {
+		//console.log("Valores: "+newValue)
+		for (var i = 0; i < newValue.length; i++) {
+			//console.log("Valor:.. "+ newValue[i])
+		}
+	});
+}])
+
+app.controller('Iteracion', ['', function($scope){
+	var self = this
+	self.msn = "Hola"
+}])
