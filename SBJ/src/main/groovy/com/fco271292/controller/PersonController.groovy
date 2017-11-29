0473b854d49d29ec359a8bbc2447d9044ef49f3b
juanimsoftware <juan.reyes@imsoftware.pro>
2017-11-29 16:00:05 -0600

    Se ajusta nombre de metodo

--- SBJ/src/main/groovy/com/fco271292/controller/PersonController.groovy
+++ SBJ/src/main/groovy/com/fco271292/controller/PersonController.groovy
@@ -1,9 +1,8 @@
 package com.fco271292.controller;
 
-import java.util.List
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fco271292.client.ClientSOAP
+import com.fco271292.model.Car
 import com.fco271292.model.Person
 import com.fco271292.repository.PersonRepository
 import com.fco271292.service.PersonService
@@ -41,8 +42,8 @@ public class PersonController {
 	
 	@RequestMapping(value = "get/{idPerson}",  method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	Person save(@PathVariable Long idPerson) {
-		Person p = personService.get(idPerson)	
+	Person get(@PathVariable Long idPerson) {
+		Person p = personService.get(idPerson)
 	}
 	
 	@RequestMapping(value = "list",  method = RequestMethod.GET, produces = "application/json")
