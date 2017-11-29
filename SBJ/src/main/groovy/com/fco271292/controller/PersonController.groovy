8a35453b220c18b00e0508e448f23d113bf8f679
juanimsoftware <juan.reyes@imsoftware.pro>
2017-11-29 13:26:05 -0600

    Se optimizan imports

--- SBJ/src/main/groovy/com/fco271292/controller/PersonController.groovy
+++ SBJ/src/main/groovy/com/fco271292/controller/PersonController.groovy
@@ -1,22 +1,20 @@
 package com.fco271292.controller;
 
+import java.util.List
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fco271292.model.Person
 import com.fco271292.repository.PersonRepository
 import com.fco271292.service.PersonService
-
-import groovy.json.*
 
 @Controller
 @RequestMapping("/persona")
