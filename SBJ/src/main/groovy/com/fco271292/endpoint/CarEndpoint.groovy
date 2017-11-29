3829113ff2da318509d22cd981f18ade4761b8cc
juanimsoftware <juan.reyes@imsoftware.pro>
2017-11-29 16:14:09 -0600

    Se cambia endpoint de Java a Groovy

--- SBJ/src/main/groovy/com/fco271292/endpoint/CarEndpoint.groovy
+++ SBJ/src/main/groovy/com/fco271292/endpoint/CarEndpoint.groovy
@@ -19,21 +19,20 @@ public class CarEndpoint {
 	@Autowired
 	CarRepository carRepository;
 	
-	private static final String NAMESPACE_URI = "http://fco271292.com/ws/carro";
+	static final String NAMESPACE_URI = "http://fco271292.com/ws/carro"
 	
 	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCarRequest")
 	@ResponsePayload
 	public GetCarResponse getCar(@RequestPayload GetCarRequest request) {
-		System.out.println("### "+this.getClass().toString()+" ###");
-		System.out.println("### "+request.getId()+" ###");
-		Car car = carRepository.findOne( Long.parseLong(request.getId()) );
-		Carro carro = new Carro();
-		carro.setMarca(car.getTradeMark());
 		
-		GetCarResponse response = new GetCarResponse();
+		Car car = carRepository.findOne( Long.parseLong(request.getId()) )
+		Carro carro = new Carro()
+		carro.setMarca(car?.getTradeMark())
+		
+		GetCarResponse response = new GetCarResponse()
 		response.setCarro(carro);
 		
-		return response;
+		response
 	}
 	
 }
--- SBJ/src/main/java/com/fco271292/ws/carro/package-info.java
+++ SBJ/src/main/java/com/fco271292/ws/carro/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.11.29 a las 02:10:03 PM CST 
+// Generado el: 2017.11.29 a las 04:07:43 PM CST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://fco271292.com/ws/carro", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
