f3b762ca0d0d2506a523e38db2ac440d21c01f51
juanimsoftware <juan.reyes@imsoftware.pro>
2017-11-30 09:41:44 -0600

    Se reubica paquete de controller en java

--- SBJ/src/main/java/com/fco271292/controller/HomeController.java
+++ SBJ/src/main/java/com/fco271292/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.fco271292.controllers;
+package com.fco271292.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
