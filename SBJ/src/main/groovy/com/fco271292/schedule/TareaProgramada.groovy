ecca51100e543222bf1182af008ad6c3fbbbac32
juanimsoftware <juan.reyes@imsoftware.pro>
2017-12-22 16:02:21 -0600

    Se agrega tarea programada cada 30 segundos

---
+++ SBJ/src/main/groovy/com/fco271292/schedule/TareaProgramada.groovy
@@ -0,0 +1,15 @@
+package com.fco271292.schedule
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
+
+@Component
+class TareaProgramada {
+	
+	@Scheduled(cron = "0/30 * * * * ?")
+	void obtenerHora() {
+		println "#"*50
+		println new Date().format('d-MM-yyyy HH:mm:ss')
+	}
+}
