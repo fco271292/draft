60a89615d7f6413c0464186226781c701c2ae70d
juanimsoftware <juan.reyes@imsoftware.pro>
2017-12-29 13:10:38 -0600

    Se agrega funcion para listar, guardar, actualizar registro

--- SBJ/src/main/groovy/com/fco271292/controller/CarController.groovy
+++ SBJ/src/main/groovy/com/fco271292/controller/CarController.groovy
@@ -3,12 +3,15 @@ package com.fco271292.controller
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
 import com.fco271292.client.ClientSOAP
 import com.fco271292.model.Car
+import com.fco271292.repository.CarRepository
 
 @Controller
 @RequestMapping("/carro")
@@ -17,10 +20,33 @@ class CarController {
 	@Autowired
 	ClientSOAP clientSOAP
 	
+	@Autowired
+	CarRepository carRepository
+	
 	@RequestMapping(value = "get/{idCar}",  method = RequestMethod.GET, produces = "application/xml")
 	@ResponseBody
 	String getCar(@PathVariable String idCar) {
 		clientSOAP.getCar(idCar)
+	}
+	
+	@RequestMapping(value = "save",  method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody
+	Car save (@RequestBody Car car) {
+		Car carro = carRepository.save(car) ?: new Car()
+	}
+	
+	@RequestMapping(value = "list",  method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	List<Car> list () {
+		List<Car> listaCarros = carRepository.findAll()
+	}
+	
+	@RequestMapping(value = "update",  method = RequestMethod.PUT, produces = "application/json")
+	@ResponseBody
+	Car update (@RequestParam id, @RequestBody Car car) {
+		Car carro = carRepository.findOne(id as Long)
+		carro.tradeMark = car?.tradeMark ?: carro?.tradeMark
+		carro = carRepository.save(carro)
 	}
 	
 }
