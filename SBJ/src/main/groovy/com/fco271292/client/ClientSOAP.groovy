1485a4e03c399179ad5ff6121ec00a3069ff2c33
juanimsoftware <juan.reyes@imsoftware.pro>
2017-12-29 16:40:18 -0600

    Se agrega la variable enviroment

--- SBJ/src/main/groovy/com/fco271292/client/ClientSOAP.groovy
+++ SBJ/src/main/groovy/com/fco271292/client/ClientSOAP.groovy
@@ -1,15 +1,25 @@
 package com.fco271292.client
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Service
 
 import wslite.soap.SOAPClient
 
 @Service
+@PropertySource("classpath:application.properties")
 class ClientSOAP {
+	
+	@Autowired
+	Environment env
 	
 	def getCar(String idCar) {
 		
-		def client = new SOAPClient('http://localhost:1234/ws/carro.wsdl')
+		String puerto = env.getProperty("server.port")
+		
+		def client = new SOAPClient("http://localhost:${puerto}/ws/carro.wsdl")
 		def response = client.send( SOAPAction: "getCar"){
 			body {
 				getCarRequest('xmlns':'http://fco271292.com/ws/carro'){
