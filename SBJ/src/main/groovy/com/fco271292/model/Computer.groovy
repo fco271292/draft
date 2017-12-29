155d6ff276e57c875f8fc9f50c90ac4e92cbfc2e
juanimsoftware <juan.reyes@imsoftware.pro>
2017-12-28 18:09:40 -0600

    Se cambia nombre de Id

--- SBJ/src/main/groovy/com/fco271292/model/Computer.groovy
+++ SBJ/src/main/groovy/com/fco271292/model/Computer.groovy
@@ -8,12 +8,10 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 
 @Document(collection='computadora')
-@JsonInclude(Include.NON_NULL)
-@JsonPropertyOrder( 'person' )
 class Computer implements Serializable{
 
 	@Id
-	String Id
+	String id
 	
 	String tradeMark
 	
