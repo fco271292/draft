a0076126dd52ecc5bc14842660bef7a8011620d5
juanimsoftware <juan.reyes@imsoftware.pro>
2017-12-29 13:12:09 -0600

    Se agrega anotacion para habilitar la auditoria de base de datos, como es fechaCreacion, fechaModificaion

--- SBJ/src/main/java/com/fco271292/SbjApplication.java
+++ SBJ/src/main/java/com/fco271292/SbjApplication.java
@@ -3,11 +3,13 @@ package com.fco271292;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.fco271292")
 @EnableScheduling
+@EnableJpaAuditing
 public class SbjApplication {
 
 	public static void main(String[] args) {
