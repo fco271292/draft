6562c4ef43313be11e160bbf2032fa8d71222495
juanimsoftware <juan.reyes@imsoftware.pro>
2018-01-04 11:28:42 -0600

    Se agrega configuracion para guardar en archivo, asi como mostrar LOG en consola con Camel

--- SBJ/src/main/groovy/com/fco271292/camel/RutaCamel.groovy
+++ SBJ/src/main/groovy/com/fco271292/camel/RutaCamel.groovy
@@ -1,5 +1,6 @@
 package com.fco271292.camel
 
+import org.apache.camel.Exchange
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
 
@@ -8,7 +9,11 @@ class RutaCamel extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-		from("timer:${this.getClass().getCanonicalName()}").to("log:${new Date().format('dd-MM-YYYY')}?level=INFO&groupInterval=5000")
+		from("timer:Temporizador?period=30s")
+			.setHeader(Exchange.FILE_NAME,constant("LOG_${new Date().format('yyyyMMdd_HHmmss')}.txt"))
+			.setBody().simple("${new Date().format('dd-MM-yyyy HH:mm:ss z')}")
+			.to("log:${this.getClass()}?level=INFO")
+			.to("file:salida/log")
 	}
 
 }
